48669240c26e4889eb1a2300064dc385c7774417
dana <danaputra1001@gmail.com>
2024-10-23 20:44:47 +0700

    gantiorigin

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5004;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gunadarma-e-comm-fe.vercel.app/",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
