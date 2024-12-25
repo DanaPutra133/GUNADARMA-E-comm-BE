5928088db0df28fefd0d241e5adde94960f7f1cc
dana <danaputra1001@gmail.com>
2024-12-25 13:14:12 +0700

    fix controler media adds bug console

--- controllers/shop/products-controller.js
+++ controllers/shop/products-controller.js
@@ -47,7 +47,7 @@ const getFilteredProducts = async (req, res) => {
       data: products,
     });
   } catch (e) {
-    console.log(error);
+    console.log(e);
     res.status(500).json({
       success: false,
       message: "Some error occured",
